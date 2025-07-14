d3f3493c72ed20d0cd8a3b16d9bac53ed85f3f28
Mohdniyam <niyamul.haque98@gmail.com>
2025-07-14 13:06:14 +0530

    change file name

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
 // server.js
 require('dotenv').config();
 const express = require('express');
-// const cors = require('cors');
-// const cookieParser = require('cookie-parser');
-// const helmet = require('helmet');
-// const morgan = require('morgan');
+
 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Connect to PostgreSQL
-// connectDB();
-
-// Middleware
-// app.use(helmet()); // security headers
-// app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
-// app.use(cookieParser());
-// app.use(morgan('dev'));
 
-// // Routes
-// app.use('/api/blogs', blogRoutes);
-// app.use('/api/auth', authRoutes);
 
 // Health check
 app.get('/', (req, res) => {
